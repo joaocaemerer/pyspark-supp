d5680a3b172ffcafd559595e672e149d9c01bbd6
Cloud2Go - Fernando Almeida Caemerer <fernando.caemerer@cloud2go.com.br>
2023-05-15 17:34:12 -0300

    Fix tags

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
  
 classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Education',
-  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'Development Status :: 1 - Development',
+  'Intended Audience :: Data Engineers',
+  'Operating System :: All',
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3'
 ]
