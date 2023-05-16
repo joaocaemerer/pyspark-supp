7491a94ec1dab18b60e7648ff61d31eb5b7f9f4b
Giancarlo Fontela da Luz <giancarlo.luz@cloud2go.com.br>
2023-05-16 16:09:03 -0300

    [no ci] feat: .gitignore, dynamic naming with env

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from os import environ
  
 long_description = 'This library was made to help Data Engineer that uses Pyspark data frames. Using functions to compare, test and check data.'
 
@@ -9,7 +10,11 @@ classifiers = [
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3'
 ]
- 
+
+pkg_name = ((environ.get('PKG_PREFIX').strip() + '.') if (environ.get('PKG_PREFIX') is not None) else '') + 'pyspark_supp'
+
+print('Package name: ' + pkg_name)
+
 setup(
   name='pyspark_supp',
   version='0.1.0',
